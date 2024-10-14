a98cb0256f0aee0a8704b11f86c723bdf59a8912
Atuldubey98 <atuldubey017@gmail.com>
2024-10-14 07:23:32 +0530

    fix : db

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,4 @@
-import dotenv from "dotenv";
-dotenv.config();
-
+import 'dotenv/config';
 import { connectDB } from "./lib/db.js";
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
@@ -10,10 +8,9 @@ const PORT = process.env.PORT || 4000;
 // console.log(process.env);
 
 app.use("/api/auth", authRoutes);
-
+connectDB()
 app.listen(PORT, () => {
   console.log(`Server is running on port : ${PORT}`);
-  connectDB();
 });
 
 // MqrdglDgOyRtPnBB
